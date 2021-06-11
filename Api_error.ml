b2569c314f9dcee9c6660cc66324ed1696755810
leQaa jamal <leqaa.abdulnasser@gmail.com>
2021-06-11 23:28:37 +0200

    changes for the version

--- Api_error.ml
+++ Api_error.ml
@@ -144,15 +144,15 @@ let catch_trace errs =
   Answer.fail (Unknown (trace_to_str errs))
 
 let catch_last_env_error err s =
-  let wrapped = Environment.wrap_tzerror err in
+  let wrapped = Environment.wrap_error_lwt err in
   match wrapped with
-  | Error e -> catch_last_error e
+  | Error_lwt e -> catch_last_error e
   | Ok _ -> Answer.fail (Unknown s)
 
 let catch_trace_env err errs s =
-  let wrapped = Environment.wrap_error_lwt err in
+  let wrapped = Environment.wrap_error err in
   match wrapped with
-  | Error_lwt e -> catch_trace (e @ errs)
+  | Error e -> catch_trace (e @ errs)
   | Ok _ -> Answer.fail (Unknown s)
 
 let pp_rejection_msg = function
