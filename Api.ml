fd9ddd0b8728dd683a1aa2d107e1d86a6a62c39b
leQaa jamal <leqaa.abdulnasser@gmail.com>
2021-06-18 18:29:45 +0200

    changes for the version

--- Api.ml
+++ Api.ml
@@ -4,7 +4,7 @@ open Tezos_client_009_PsFLoren.Protocol_client_context
 open Tezos_client_009_PsFLoren.Injection
 open Tezos_client_009_PsFLoren.Client_proto_contracts
 open Tezos_protocol_009_PsFLoren.Protocol.Alpha_context
-open Tezos_protocol_009_PsFLoren.Protocol.Main
+open Tezos_protocol_009_PsFLoren.Protocol.Apply_resultss
 open Tezos_raw_protocol_009_PsFLoren
 open Tezos_protocol_environment_009_PsFLoren
 open Apply_results
@@ -416,7 +416,7 @@ let query oph =
       >>= function
       | Ok op -> (
          match (op.receipt, op.protocol_data) with
-         | (Apply_operation.Operation_metadata omd, Operation_data od) ->
+         | (Operation_metadata omd, Operation_data od) ->
             begin
               match Apply_results.kind_equal_list od.contents omd.contents with
               | Some Apply_results.Eq ->
