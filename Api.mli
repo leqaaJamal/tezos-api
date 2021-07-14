375912aca19aaa388837a735e4f30cc38ea20833
leQaa jamal <leqaa.abdulnasser@gmail.com>
2021-07-15 01:18:51 +0200

    trying get_balance1

--- Api.mli
+++ Api.mli
@@ -206,4 +206,4 @@ val get_float : Tez_t.t -> unit
 
 val get_parmeter_type: string -> (string * Script.expr) list Answer.t
 
-val print_elements: (tag * 'a) list -> unit
+(* val print_elements: (tag * 'a) list -> unit *)
