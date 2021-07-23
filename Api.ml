fcb8ef04612425f0425b5ebef3cafdb611b16540
leQaa jamal <leqaa.abdulnasser@gmail.com>
2021-07-24 01:28:25 +0200

    trying types

--- Api.ml
+++ Api.ml
@@ -642,8 +642,8 @@ let check_type entrypointname contr arg =
             (* Answer.return true *)
             (* Stdlib.print_endline stringty *)
             if Int64.of_int (String.compare stringty argty) = Int64.zero
-            then Answer.return true
-            else Answer.return false
+            then Answer.return "true"
+            else Answer.return "false"
           )
         )
     )
