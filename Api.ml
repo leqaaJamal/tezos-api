4d6bffbbdea0771bdcf1a0ac8ad1ac2e7a34dc90
leQaa jamal <leqaa.abdulnasser@gmail.com>
2021-07-23 22:14:31 +0200

    trying types

--- Api.ml
+++ Api.ml
@@ -639,8 +639,7 @@ let check_type entrypointname contr arg =
         (
           let argty = mtype_to_string arg in 
           (
-            Stdlib.print_endline stringty;
-            Answer.return true
+            Stdlib.print_endline stringty
             (* if stringty = argty
             then Answer.return true
             else Answer.return false *)
