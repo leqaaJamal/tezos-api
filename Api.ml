c2593266de339c4b49123ddefa57e61439bd57ca
leQaa jamal <leqaa.abdulnasser@gmail.com>
2021-07-25 03:09:25 +0200

    trying types

--- Api.ml
+++ Api.ml
@@ -624,18 +624,18 @@ let print_entrypoints entrylist =
 let string_of_expression ~zero_loc expression =
   let show_loc loc = if zero_loc then 0 else loc in
   let string_of_node = function
-    | Int (loc, i) ->
+    |Int (loc, i) ->
         Format.asprintf "Int"
-    | String (loc, s) ->
+    |String (loc, s) ->
         Format.asprintf "String"
-    | Bytes (loc, b) ->
+    |Bytes (loc, b) ->
         Format.asprintf
           "Bytes"
-    | Prim (loc, prim, nodes, annot) ->
+    |Prim (loc, prim, nodes, annot) ->
         Format.asprintf
           "%s"
           (ocaml_constructor_of_prim prim)
-    | Seq (loc, nodes) ->
+    |Seq (loc, nodes) ->
         Format.asprintf
           "Seq"
   in
