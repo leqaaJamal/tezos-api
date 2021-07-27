979b211b9ca804e0f025d22d8aad4c334956f0d8
leQaa jamal <leqaa.abdulnasser@gmail.com>
2021-07-27 14:47:25 +0200

    trying types

--- Api.mli
+++ Api.mli
@@ -235,4 +235,4 @@ val print_entrypoints: (tag * Script.expr) list tzresult -> unit tzresult Lwt.t
 (* val print_elements_types: (tag * Script.expr) list -> (expression_michelson -> unit) *)
 val string_of_expression: Script.expr -> string
 
-val call_contract : Tez_t.t -> pukh -> contract -> ?entrypoint:string -> ?arg:mtype -> Tez_t.t -> oph Answer.t
+val call_contract1 : Tez_t.t -> pukh -> contract -> ?entrypoint:string -> ?arg:mtype -> Tez_t.t -> oph Answer.t
