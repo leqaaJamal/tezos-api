b22f14a9d54b2587f613dc55020f5189b7ac6b79
leQaa jamal <leqaa.abdulnasser@gmail.com>
2021-07-27 16:24:42 +0200

    trying types

--- Api.ml
+++ Api.ml
@@ -704,7 +704,7 @@ let call_contract1 amount src destination ?entrypoint ?arg fee =
   | Ok (_, src_pk, src_sk) ->
      begin
      (* here should check the type and change the arg to string *)
-      let check = check_type (entrypoint_to_string entrypoint) destination (arg_to_mtype arg) in (
+      let check = check_type (entrypoint_to_string ?entrypoint) destination (arg_to_mtype ?arg) in (
         if Int64.of_int (String.compare check "true") = Int64.zero
         then (
           let argvalue = value_to_string (arg_to_mtype ?arg) in
