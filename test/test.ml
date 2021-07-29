47dd08a289199945969660dffaa701d02230386c
leQaa jamal <leqaa.abdulnasser@gmail.com>
2021-07-29 21:50:00 +0200

    trying types

--- test/test.ml
+++ test/test.ml
@@ -152,13 +152,11 @@ let run_check_entrypointty () =
 
 let run_check_entrypointty1 () =
   Api.get_contract "id1"
-  >>=? fun contr ->(
-    let x = "bla" in 
-  Api.check_type1 "default" contr ?x ()
+  >>=? fun contr -> 
+  Api.check_type1 "default" contr ?arg="bla" ()
   >>= function
     | Ok out -> print_endline "Ok"; print_endline out; Lwt.return_ok ()
     | Error err -> Lwt.return_error err
-  )
 
 
 let run_get_code () =
