92ebc02f526f3ae8ab16eb915606361d6a75f65c
leQaa jamal <leqaa.abdulnasser@gmail.com>
2021-07-29 20:49:36 +0200

    trying types

--- Api.ml
+++ Api.ml
@@ -686,7 +686,7 @@ let check_type entrypointname contr arg =
 let get_expr_from_lexpr lexpr = 
   match Script_repr.force_decode lexpr with
   | Ok (expr,_) -> Answer.return expr 
-  | Error err -> cctxt#error "%a" Environment.Error_monad.pp_trace errs
+  | Error err -> ctxt#error "%a" Environment.Error_monad.pp_trace errs
 
 let check_type1 entrypointname contr ?arg =
   let ctxt_rpc = new wrap_full !ctxt in 
