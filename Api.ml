0e884878f53778dc42bcd40668e157ee9c22d889
leQaa jamal <leqaa.abdulnasser@gmail.com>
2021-07-29 21:08:29 +0200

    trying types

--- Api.ml
+++ Api.ml
@@ -724,7 +724,7 @@ let check_type1 entrypointname contr ?arg =
                 else Answer.return "false"
               )
             )
-            | Error err -> ""
+            | Error err -> Answer.fail err
             
           )
           
