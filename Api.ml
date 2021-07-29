8d7cd379d87c89e981b199a49e00025318a25cfc
leQaa jamal <leqaa.abdulnasser@gmail.com>
2021-07-29 17:42:12 +0200

    trying types

--- Api.ml
+++ Api.ml
@@ -716,7 +716,7 @@ let check_type1 entrypointname contr arg =
                 else Answer.return "false"
               )
             )
-            | None -> error Rejected (Unknown_reason "types do not match")
+            | None -> error (Rejected (Unknown_reason "types do not match"))
             
           )
           | Error err -> catch_error_f err
