46797f87cdd266f764cdc690302cf2aa3b278b55
leQaa jamal <leqaa.abdulnasser@gmail.com>
2021-07-30 22:31:43 +0200

    trying types

--- Api.ml
+++ Api.ml
@@ -711,7 +711,7 @@ let parse_arg_transfer arg =
 
 let try1 ?arg () =
   parse_arg_transfer arg >>=? fun expr ->(
-    asprintf "%a" (string_of_expression expr)
+    (string_of_expression expr)
   )
 
 
