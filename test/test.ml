b179b29fa6c1d93f6d59703429425b4e2f11a04f
leQaa jamal <leqaa.abdulnasser@gmail.com>
2021-07-30 22:37:51 +0200

    trying types

--- test/test.ml
+++ test/test.ml
@@ -159,7 +159,7 @@ let run_check_entrypointty () =
     | Error err -> Lwt.return_error err *)
 
 let run_try1 () =
-  Api.try1 ?arg:(Some "x") ()
+  Api.try1 ?arg:(Some "string") ()
   >>= function
     | Ok out -> print_endline "Ok"; print_endline out; Lwt.return_ok ()
     | Error err -> Lwt.return_error err
