47bf1c1da7f3726de2edc28dca1380a772191e32
leQaa jamal <leqaa.abdulnasser@gmail.com>
2021-08-01 23:16:00 +0200

    trying types

--- test/test.ml
+++ test/test.ml
@@ -165,11 +165,11 @@ let run_try1 () =
     | Error err -> Lwt.return_error err
 
 
-let run_mtype1_to_string () =
+(* let run_mtype1_to_string () =
   Api.mtype1_to_string (Tstring "\"ss\"")
   >>= function
     | Ok out -> print_endline "Ok"; print_endline out; Lwt.return_ok ()
-    | Error err -> Lwt.return_error err
+    | Error err -> Lwt.return_error err *)
 
 
 let run_get_code () =
@@ -238,9 +238,9 @@ let main =
     >>=? fun _ ->
     print_endline "Test try1";
     run_try1 ()
-    >>=? fun _ ->
+    (* >>=? fun _ ->
     print_endline "Test try1";
-    run_mtype1_to_string ()
+    run_mtype1_to_string () *)
     (* >>=? fun _ ->
     print_endline "Test check entrypointty";
     run_check_entrypointty1 () *)
