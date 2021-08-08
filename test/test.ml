d7dd3f6c488df538fafb0a8b28319c60b818b2f0
leQaa jamal <leqaa.abdulnasser@gmail.com>
2021-08-08 18:36:09 +0200

    trying types

--- test/test.ml
+++ test/test.ml
@@ -135,7 +135,71 @@ let run_get_print_code () =
   "parameter (int); \n\
    storage (int); \n\n\
    code\n\
-   {UNIT ; NIL operation ; PAIR } \n"
+  \  {\n\
+  \    UNPAIR ; SWAP ; DUP ; DIP { SWAP } ;\n\
+  \    DIP\n\
+  \      {\n\
+  \        UNPAIR ;\n\
+  \        # pair the payload with the current contract address, to ensure \
+   signatures\n\
+  \        # can't be replayed across different contracts if a key is reused.\n\
+  \        DUP ; SELF ; ADDRESS ; CHAIN_ID ; PAIR ; PAIR ;\n\
+  \        PACK ; # form the binary payload that we expect to be signed\n\
+  \        DIP { UNPAIR @counter ; DIP { SWAP } } ; SWAP\n\
+  \      } ;\n\n\
+  \    # Check that the counters match\n\
+  \    UNPAIR @stored_counter; DIP { SWAP };\n\
+  \    ASSERT_CMPEQ ;\n\n\
+  \    # Compute the number of valid signatures\n\
+  \    DIP { SWAP } ; UNPAIR @threshold @keys;\n\
+  \    DIP\n\
+  \      {\n\
+  \        # Running count of valid signatures\n\
+  \        PUSH @valid nat 0; SWAP ;\n\
+  \        ITER\n\
+  \          {\n\
+  \            DIP { SWAP } ; SWAP ;\n\
+  \            IF_CONS\n\
+  \              {\n\
+  \                IF_SOME\n\
+  \                  { SWAP ;\n\
+  \                    DIP\n\
+  \                      {\n\
+  \                        SWAP ; DIIP { DUUP } ;\n\
+  \                        # Checks signatures, fails if invalid\n\
+  \                        { DUUUP; DIP {CHECK_SIGNATURE}; SWAP; IF {DROP} \
+   {FAILWITH} };\n\
+  \                        PUSH nat 1 ; ADD @valid } }\n\
+  \                  { SWAP ; DROP }\n\
+  \              }\n\
+  \              {\n\
+  \                # There were fewer signatures in the list\n\
+  \                # than keys. Not all signatures must be present, but\n\
+  \                # they should be marked as absent using the option type.\n\
+  \                FAIL\n\
+  \              } ;\n\
+  \            SWAP\n\
+  \          }\n\
+  \      } ;\n\
+  \    # Assert that the threshold is less than or equal to the\n\
+  \    # number of valid signatures.\n\
+  \    ASSERT_CMPLE ;\n\
+  \    DROP ; DROP ;\n\n\
+  \    # Increment counter and place in storage\n\
+  \    DIP { UNPAIR ; PUSH nat 1 ; ADD @new_counter ; PAIR} ;\n\n\
+  \    # We have now handled the signature verification part,\n\
+  \    # produce the operation requested by the signers.\n\
+  \    NIL operation ; SWAP ;\n\
+  \    IF_LEFT\n\
+  \      { # Transfer tokens\n\
+  \        UNPAIR ; UNIT ; TRANSFER_TOKENS ; CONS }\n\
+  \      { IF_LEFT {\n\
+  \                  # Change delegate\n\
+  \                  SET_DELEGATE ; CONS }\n\
+  \                {\n\
+  \                  # Change set of signatures\n\
+  \                  DIP { SWAP ; CAR } ; SWAP ; PAIR ; SWAP }} ;\n\
+  \    PAIR }\n"
   >>= function 
     | Ok ans -> print_endline ans; print_endline "Ok" ; Lwt.return_ok ()
     | Error err -> Lwt.return_error err
