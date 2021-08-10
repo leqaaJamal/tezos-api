156c9c048fae992b68b1f7bf3aded6d0083c4a53
leQaa jamal <leqaa.abdulnasser@gmail.com>
2021-08-10 03:43:59 +0200

    trying types

--- Api.ml
+++ Api.ml
@@ -902,8 +902,8 @@ let print_code s =
   parse_script s  >>=? fun parsed ->
   (* Answer.return parsed *)
   (* Answer.return parsed.source *)
-  (* Answer.return (Michelson_v1_printer.micheline_string_of_expression ~zero_loc:false parsed.expanded) *)
-  Answer.return (micheline_string_of_expression parsed.expanded)
+  Answer.return (Michelson_v1_printer.micheline_string_of_expression ~zero_loc:false parsed.expanded)
+  (* Answer.return (micheline_string_of_expression parsed.expanded) *)
 
 (* val originate: string -> Tez_t.t -> pukh -> string ->
   (Kind.origination Kind.manager Injection.result * Contract.t) Answer.t *)
