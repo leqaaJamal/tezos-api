52650f737638f618f107a9e5d1bb65e34563e47b
leQaa jamal <leqaa.abdulnasser@gmail.com>
2021-08-18 00:49:41 +0200

    trying types

--- test/test.ml
+++ test/test.ml
@@ -249,7 +249,7 @@ let run_try1 () =
     | Error err -> Lwt.return_error err *)
 
 let run_value_to_string () =
-  let out = Api.value_to_string (Tstring "xxx") in 
+  let out = Api.value_to_string (Bool True) in 
   (
   print_endline "Ok";
   print_endline out;
