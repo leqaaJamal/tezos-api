a7898104395407095ed27db4f1522abb03cfd61d
leQaa jamal <leqaa.abdulnasser@gmail.com>
2021-08-22 19:50:47 +0200

    trying types

--- test/test.ml
+++ test/test.ml
@@ -374,9 +374,9 @@ let main =
     >>=? fun _ ->
     print_endline "Test run_check_storage_type";
     run_check_storage_type ()
-    >>=? fun _ ->
+    (* >>=? fun _ ->
     print_endline "Test call_contract";
-    run_call_contract ()
+    run_call_contract () *)
     >>=? fun _ ->
     print_endline "Test call_contract2";
     run_call_contract2 ()
